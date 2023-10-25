02ca3c082eda9554a384c21de41a4a462fdf9dbd
LiuLinjie0310 <137786459+LiuLinjie0310@users.noreply.github.com>
2023-10-25 15:01:34 +0800

    Update CrossUNet_ECAskip.py

--- CrossUNet_ECAskip.py
+++ CrossUNet_ECAskip.py
@@ -8,7 +8,7 @@ from einops import rearrange
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 from einops.layers.torch import Rearrange
 import numpy as np
-from Models.ECAAttention import ECAAttention
+from ECAAttention import ECAAttention
 
 class Mlp(nn.Module):
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
